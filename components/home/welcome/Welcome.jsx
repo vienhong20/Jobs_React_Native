92a61c0e0e9abfac7c434accd5d6a0a6f99339da
VIEN HONG <vienhong88@gmail.com>
2023-03-22 14:32:30 -0400

    Update Fonts

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useState } from 'react'
+import { View, Text, TextInput, TouchableOpacity, Image, FlatList } from 'react-native';
+import { useRouter } from 'expo-router';
 
-import styles from './welcome.style'
+import styles from './welcome.style';
+import { icons, SIZES } from '../../../constants';
 
 const Welcome = () => {
+  const router = useRouter();
+
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Vince</Text>
+        <Text style={styles.welcomeMessage}>Find Your Perfect Job</Text> 
+      </View>
+
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput 
+            style={styles.searchInput}
+          />
+
+        </View>
+      </View>
     </View>
   )
 }
