6e5b3b21be5e98c8851577dcb1c89fb53efa6426
VIEN HONG <vienhong88@gmail.com>
2023-03-25 19:10:01 -0400

    Update

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,28 +1,16 @@
-import { useState } from 'react';
-import { useRouter } from 'expo-router';
-import { View, Text, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native';
+import { useRouter } from 'expo-router'
+import { View, Text , TouchableOpacity, FlatList, ActivityIndicator } from 'react-native';
 
 import styles from './popularjobs.style'
-import{ COLORS, SIZES } from '../../../constants';
+import { COLORS, SIZES } from '../../../constants';
 import PopularJobCard from '../../common/cards/popular/PopularJobCard';
 
-import useFetch from '../../../hook/useFetch';
+
 
 const Popularjobs = () => {
   const router = useRouter();
-  
-  const { data, isLoading, error } = useFetch('search', {
-    query: 'React developer',
-    num_pages: "1",
-  });
-
-  const [selectedJob, setSelectedJob] = useState();
-
-  const handleCardPress = (item) => {
-      router.push(`/job-details/${item.job_id}`);
-      setSelectedJob(item.job_id);
-  };
-  //console.log(data);
+  const isLoading = false;
+  const error = false;
 
   return (
     <View style={styles.container}>
@@ -35,22 +23,18 @@ const Popularjobs = () => {
 
       <View style={styles.cardsContainer}>
         {isLoading ? (
-          <ActivityIndicator size="large" color={COLORS.primary} />
+          <ActivityIndicator size="large" colors={COLORS.primary} />
         ) : error ? (
           <Text>Something went wrong</Text>
         ) : (
           <FlatList 
-            data={data} 
+            data={[1,2,3,4,5]}
             renderItem={({ item }) => (
-              <PopularJobCard
-                item={item} 
-                selectedJob={selectedJob}
-                handleCardPress={handleCardPress}
-              />
-            )}
-            keyExtractor={(item) => item.job_id}
-            contentContainerStyle={{ columnGap: SIZES.medium }}
-            horizontal
+            <PopularJobCard item={item} />
+          )}
+          keyExtractor={item => item?.job_id}
+          contentContainerStyle={{ columnGap: SIZES.medium }}
+          horizontal
           />
         )}
       </View>
@@ -58,4 +42,4 @@ const Popularjobs = () => {
   )
 }
 
-export default Popularjobs;
+export default Popularjobs
